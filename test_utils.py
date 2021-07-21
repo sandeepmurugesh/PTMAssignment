d3a147c8a6137082fcf7065d17ede30223195318
sandeep <sandeepmurugesh@gmail.com>
2021-07-21 17:46:21 -0400

    push

--- test_utils.py
+++ test_utils.py
@@ -12,13 +12,3 @@ class UtilsTestCase(TestCase):
         sample_df = load_csv_df(self.spark,"data/sample.csv")
         result_count = sample_df.count()
         self.assertEqual(result_count,9,"Record count should be 9")
-    def test_country_count(self):
-        sample_df = load_csv_df(self.spark,"data/sample.csv")
-        count_list = get_country_count(sample_df).collect()
-
-        count_dict = dict()
-        for row in count_list:
-            count_dict[row["Country"]] = row["count"]
-        self.assertEqual(count_dict["United States"],4,"Count for Us is 4")
-        self.assertEqual(count_dict["Canada"],2,"Count for canada is 2")
-        self.assertEqual(count_dict["United Kingdom"],1,"Count for UK is 1")
